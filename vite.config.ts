2a67347394db05c6f3c6694f7e1cff7e855812bd
peacemaintenancemaster <mtsun97@gmail.com>
2026-02-10 16:12:52 +0900

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { reactRouter } from '@react-router/dev/vite';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+	plugins: [reactRouter(), tsconfigPaths(), svgr()],
+	server: {
+		port: 7021,
+	},
+});
